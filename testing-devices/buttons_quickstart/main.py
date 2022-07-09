a555d82ae41f01b771d78bb95fceae76db2f756a
T. K. Stenczel <tks32@cam.ac.uk>
2022-07-09 13:53:54 +0100

    demo 0

--- testing-devices/buttons_quickstart/main.py
+++ testing-devices/buttons_quickstart/main.py
@@ -1,59 +1,67 @@
 #!/usr/bin/env pybricks-micropython
+import time
 
+from pybricks.ev3devices import (
+    ColorSensor,
+    GyroSensor,
+    Motor,
+    TouchSensor,
+    UltrasonicSensor,
+)
 from pybricks.hubs import EV3Brick
-from pybricks.parameters import Button
+from pybricks.parameters import Port
+
+# declaration
+TEST_MOTORS = True
+TEST_SENSORS = True
 
 
-# Initialize the EV3.
+# init
 ev3 = EV3Brick()
+ev3.speaker.beep()
 
 
-def wait_for_button(ev3):
-    """
-    This function shows a picture of the buttons on the EV3 screen.
+def test_sensor(sensor):
+    start = time.perf_counter()
+    while True:
+        ev3.screen.clear()
 
-    Then it waits until you press a button.
+        if isinstance(sensor, TouchSensor):
+            ev3.screen.draw_text(2, 100, "btn: " + str(sensor.pressed()))
+        elif isinstance(sensor, UltrasonicSensor):
+            ev3.screen.draw_text(2, 100, "dist: " + str(sensor.distance()))
+        elif isinstance(sensor, ColorSensor):
+            ev3.screen.draw_text(2, 100, "Color: " + str(sensor.color()))
+        elif isinstance(sensor, GyroSensor):
+            ev3.screen.draw_text(2, 100, "angle: " + str(sensor.angle()))
 
-    It returns which button was pressed.
-    """
-
-    # Show a picture of the buttons on the screen.
-    ev3.screen.load_image("buttons.png")
-
-    # Tip: add text or icons to the image to help you
-    # remember what each button will do in your program.
-
-    # Wait for a single button to be pressed and save the result.
-    pressed = []
-    while len(pressed) != 1:
-        pressed = ev3.buttons.pressed()
-    button = pressed[0]
-
-    # Print which button was pressed
-    ev3.screen.draw_text(2, 100, button)
-
-    # Now wait for the button to be released.
-    while any(ev3.buttons.pressed()):
-        pass
-
-    # Return which button was pressed.
-    return button
+        time.sleep(0.1)
+        if abs(time.perf_counter() - start) > 5:
+            break
 
 
-while True:
-    # Show the menu and wait for one button to be selected.
-    button = wait_for_button(ev3)
+if TEST_MOTORS:
+    # devices
+    motor_a = Motor(Port.A)
+    motor_b = Motor(Port.B)
+    motor_c = Motor(Port.C)
 
-    # Now you can do something, based on which button was pressed.
+    # Run the motor up to 500 degrees per second. To a target angle of 90 degrees.
+    motor_a.run_target(500, 90)
+    motor_b.run_target(500, 90)
+    motor_c.run_target(500, 90)
 
-    # In this demo, we just play a different sound for each button.
-    if button == Button.LEFT:
-        ev3.speaker.beep(200)
-    elif button == Button.RIGHT:
-        ev3.speaker.beep(400)
-    elif button == Button.UP:
-        ev3.speaker.beep(600)
-    elif button == Button.DOWN:
-        ev3.speaker.beep(800)
-    elif button == Button.CENTER:
-        ev3.speaker.beep(1000)
+    ev3.speaker.beep(1000, 500)
+
+if TEST_SENSORS:
+    ts = TouchSensor(Port.S1)
+    gyro = GyroSensor(Port.S2)
+    ultrasonic = UltrasonicSensor(Port.S3)
+    color_sensor = ColorSensor(Port.S4)
+
+    test_sensor(ts)
+    test_sensor(gyro)
+    test_sensor(ultrasonic)
+    test_sensor(color_sensor)
+
+    ev3.speaker.beep(1000, 500)
